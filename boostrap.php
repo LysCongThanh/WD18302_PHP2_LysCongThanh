cfe881c26cfa0a2c2437421c5dcd90e6d0f58c40
LysCongThanh <congthanh1770@gmail.com>
2024-01-02 22:14:35 +0700

    chore: Fix path issue. describle: fix path in Application class instantiation using dirname(__DIR__)

--- boostrap.php
+++ boostrap.php
@@ -7,5 +7,4 @@ require_once __DIR__.'/vendor/autoload.php';
 $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 $config = require_once __DIR__ . '/configs/kernel.php';
-
-return new Application(dirname(__DIR__), $config);
+return new Application(__DIR__, $config);
