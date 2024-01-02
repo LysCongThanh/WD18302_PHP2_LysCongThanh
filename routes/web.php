28387cf0c990c9256a8580894cacf52d58076c71
LysCongThanh <congthanh1770@gmail.com>
2024-01-02 22:11:49 +0700

    chore: Update routing

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use app\controllers\HomeController;
 
-$app->router->get('/', [HomeController::class, 'index']);
-$app->router->post('/', [HomeController::class, 'index']);
-
 $app->router->get('/login', [\app\controllers\AuthController::class, 'login']);
 $app->router->post('/login', [\app\controllers\AuthController::class, 'login']);
 $app->router->get('/register', [\app\controllers\AuthController::class, 'register']);
